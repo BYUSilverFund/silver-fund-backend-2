18a049565ee20e06f3a9fd15c5fcaf45441e69e1
Andrew Hall <andrewmartinhall2@gmail.com>
2024-05-06 17:12:06 -0600

    Added service and database classes

---
+++ database/database.py
@@ -0,0 +1,4 @@
+class Database:
+
+  def getData():
+    return "Some data"
---
+++ service/service.py
@@ -0,0 +1,7 @@
+from database.database import Database
+
+class Service:
+
+  def handleData():
+    data = Database.getData()
+    return data
