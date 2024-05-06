44010e0150e4621d619c05dfa835b85b9827005d
Andrew Hall <andrewmartinhall2@gmail.com>
2024-05-06 17:17:33 -0600

    Restructured api file

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-from flask import Flask
-from service.service import Service
+from api.api import app
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/data")
-def data():
-    data = Service.handleData()
-    return data
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=False)
