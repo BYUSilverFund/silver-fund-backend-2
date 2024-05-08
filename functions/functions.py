2206e8af1dac54ff1d4be72da17df4c36aefc575
Andrew Hall <andrewmartinhall2@gmail.com>
2024-05-08 10:11:22 -0600

    Tracking error and information ratio

--- functions/functions.py
+++ functions/functions.py
@@ -2,6 +2,8 @@ from typing import List
 import statsmodels.api as sm
 import numpy as np
 
+TRADING_DAYS = 252
+
 def portfolio_alpha(xs_port_returns: np.ndarray, xs_bmk_returns: np.ndarray) -> float:
 
   xs_port_returns = sm.add_constant(xs_port_returns)
@@ -30,4 +32,20 @@ def portfolio_volatility(port_returns: np.ndarray) -> float:
 
   standard_deviation = port_returns.std()
 
-  return standard_deviation * np.sqrt(250)
+  return standard_deviation * np.sqrt(TRADING_DAYS)
+
+
+def portfolio_tracking_error(port_returns: np.ndarray, bmk_returns: np.ndarray) -> float:
+
+  difference = port_returns - bmk_returns
+
+  return difference.std() * np.sqrt(TRADING_DAYS)
+
+
+def portfolio_information_ratio(port_returns: np.ndarray, bmk_returns: np.ndarray) -> float:
+
+  difference = port_returns - bmk_returns
+
+  tracking_error = tracking_error(port_returns,bmk_returns)
+  
+  return difference / tracking_error
