f88b37e191f3f29305ba8e321006f4879fde216e
Andrew Hall <andrewmartinhall2@gmail.com>
2024-05-08 09:57:09 -0600

    Portfolio volatility function

--- functions/functions.py
+++ functions/functions.py
@@ -1,7 +1,8 @@
 from typing import List
 import statsmodels.api as sm
+import numpy as np
 
-def portfolio_alpha(xs_port_returns: List[float], xs_bmk_returns: List[float]) -> float:
+def portfolio_alpha(xs_port_returns: np.ndarray, xs_bmk_returns: np.ndarray) -> float:
 
   xs_port_returns = sm.add_constant(xs_port_returns)
 
@@ -12,13 +13,21 @@ def portfolio_alpha(xs_port_returns: List[float], xs_bmk_returns: List[float]) -
 
   return alpha
 
-def portfolio_beta(xs_port_returns: List[float], xs_bmk_returns: List[float]) -> float:
+
+def portfolio_beta(xs_port_returns: np.ndarray, xs_bmk_returns: np.ndarray) -> float:
 
   xs_port_returns = sm.add_constant(xs_port_returns)
 
   model = sm.OLS(xs_bmk_returns, xs_port_returns)
   results = model.fit()
 
-  alpha = results.params[1]
+  beta = results.params[1]
 
-  return alpha
+  return beta
+
+
+def portfolio_volatility(port_returns: np.ndarray) -> float:
+
+  standard_deviation = port_returns.std()
+
+  return standard_deviation * np.sqrt(250)
