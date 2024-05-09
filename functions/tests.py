fa5df59f601b35dc6ce0d1690a9d43241cb5a448
Andrew Hall <andrewmartinhall2@gmail.com>
2024-05-09 13:07:56 -0600

    Improved test file

--- functions/tests.py
+++ functions/tests.py
@@ -6,16 +6,11 @@ from functions import portfolio_alpha, portfolio_beta
 def test_portfolio_alpha():
   np.random.seed(0)
   X = [x for x in range(50)]
-  y = [2*x for x in range(50)]
+  y = [x+1 for x in range(50)]
 
-  X = sm.add_constant(X)
+  true_alpha = 1
 
-  model = sm.OLS(y, X)
-  results = model.fit()
-
-  true_alpha = results.params[0]
-
-  test_alpha = portfolio_alpha(X,y)
+  test_alpha = round(portfolio_alpha(X,y))
 
   assert test_alpha == true_alpha
 
@@ -24,13 +19,8 @@ def test_portfolio_beta():
   X = [x for x in range(50)]
   y = [2*x for x in range(50)]
 
-  X = sm.add_constant(X)
+  true_beta = 2
 
-  model = sm.OLS(y, X)
-  results = model.fit()
-
-  true_beta = results.params[1]
-
-  test_beta = portfolio_beta(X,y)
+  test_beta = round(portfolio_beta(X,y))
 
   assert test_beta == true_beta
