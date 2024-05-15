6897dcb0481139332704c85010cb41ade2013283
Andrew Hall <andrewmartinhall2@gmail.com>
2024-05-15 10:33:31 -0600

    Rough version of IBKR ETL proccess

--- chron/database.py
+++ chron/database.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import psycopg2
+from sqlalchemy import create_engine
+import pandas as pd
 
 class Database():
 
@@ -9,20 +11,20 @@ class Database():
     load_dotenv()
 
     # Fetch the RDS details from environment variables
-    db_endpoint = os.getenv('DB_ENDPOINT')
-    db_name = os.getenv('DB_NAME')
-    db_user = os.getenv('DB_USER')
-    db_password = os.getenv('DB_PASSWORD')
-    db_port = os.getenv('DB_PORT', '5432')
+    self.db_endpoint = os.getenv('DB_ENDPOINT')
+    self.db_name = os.getenv('DB_NAME')
+    self.db_user = os.getenv('DB_USER')
+    self.db_password = os.getenv('DB_PASSWORD')
+    self.db_port = os.getenv('DB_PORT', '5432')
 
     try:
         # Establish the connection
         self.connection = psycopg2.connect(
-            host=db_endpoint,
-            database=db_name,
-            user=db_user,
-            password=db_password,
-            port=db_port
+            host=self.db_endpoint,
+            database=self.db_name,
+            user=self.db_user,
+            password=self.db_password,
+            port=self.db_port
         )
 
         # Create a cursor object
@@ -44,3 +46,19 @@ class Database():
     result =  self.cursor.fetchone()
 
     return result 
+  
+  def load(self, df, table_name):
+    # Create the SQLAlchemy engine
+    db_url = f'postgresql+psycopg2://{self.db_user}:{self.db_password}@{self.db_endpoint}:{self.db_port}/{self.db_name}'
+    engine = create_engine(db_url)
+
+    try:
+        # Using the `to_sql` method to upload the DataFrame
+        df.to_sql(table_name, engine, if_exists='replace', index=False)
+        print(f"Table '{table_name}' created successfully and data inserted.")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    finally:
+        engine.dispose()
---
+++ chron/notes.md
@@ -0,0 +1,12 @@
+To run this module run the following from the CLI
+```
+python chron
+```
+You need a .env file in the root directory with the following in order to establish the database connection
+```
+DB_ENDPOINT
+DB_NAME
+DB_USER
+DB_PASSWORD
+DB_PORT
+```
