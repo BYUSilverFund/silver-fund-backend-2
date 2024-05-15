fce8f9b5a836f5fc10ccc167824d072acd80831d
Andrew Hall <andrewmartinhall2@gmail.com>
2024-05-15 09:12:01 -0600

    Chron files

--- chron/ibkr_query.py
+++ chron/ibkr_query.py
@@ -4,25 +4,25 @@ import time
 from io import StringIO
 import pandas as pd
 
-def ibkr_query(token, query_id):
+def ibkr_query(token, query):
 
   # Checks
   if token is None:
     print('No token specified')
     return
   
-  if query_id is None:
+  if query is None:
     print('No query id specified')
     return
     
   # Request 1
-  url = f'https://ndcdyn.interactivebrokers.com/AccountManagement/FlexWebService/SendRequest?t={token}&q={query_id}&v=3'
+  url = f'https://ndcdyn.interactivebrokers.com/AccountManagement/FlexWebService/SendRequest?t={token}&q={query}&v=3'
   user_agent = {'User-agent': 'Python/3.9'}
   response = requests.get(url, headers=user_agent)
   reference_code = re.findall('(?<=<ReferenceCode>)\d*(?=<\/ReferenceCode>)',response.text)[0]
 
   # Request 2
-  time.sleep(10)
+  time.sleep(2)
   url = f'https://ndcdyn.interactivebrokers.com/AccountManagement/FlexWebService/GetStatement?t={token}&q={reference_code}&v=3'
   response = requests.get(url)
 
