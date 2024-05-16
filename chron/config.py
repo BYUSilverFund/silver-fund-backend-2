a058350ad8f1cb83ac89a5efa5cdff86d09c7035
Andrew Hall <andrewmartinhall2@gmail.com>
2024-05-16 09:05:34 -0600

    Used PYCharm to reformat codebase for Chron

--- chron/config.py
+++ chron/config.py
@@ -1,31 +1,31 @@
 config = {
-  "grad":{
-    "token": 819997527277769608738195,
-    "queries":{
-      "delta_nav":987356,
-      "positions": 987359,
-      "dividends": 987361,
-      "trades": 987363,
-    }
-  },
-  "undergrad":{
-    "token": 155834512513051746551265,
-    "queries":{
-      "nav": 989615,
-      "delta_nav":989561,
-      "positions": 989564,
-      "dividends": 989565,
-      "trades": 989567,
-    }
-  },
-  "brigham_capital":{
-    "token": 170229039687872048124200,
-    "queries":{
-      "nav": 989606,
-      "delta_nav":989601,
-      "positions": 989602,
-      "dividends": 989603,
-      "trades": 989605,
-    }
-  },
+    "grad": {
+        "token": 819997527277769608738195,
+        "queries": {
+            "delta_nav": 987356,
+            "positions": 987359,
+            "dividends": 987361,
+            "trades": 987363,
+        }
+    },
+    "undergrad": {
+        "token": 155834512513051746551265,
+        "queries": {
+            "nav": 989615,
+            "delta_nav": 989561,
+            "positions": 989564,
+            "dividends": 989565,
+            "trades": 989567,
+        }
+    },
+    "brigham_capital": {
+        "token": 170229039687872048124200,
+        "queries": {
+            "nav": 989606,
+            "delta_nav": 989601,
+            "positions": 989602,
+            "dividends": 989603,
+            "trades": 989605,
+        }
+    },
 }
