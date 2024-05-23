ad3d215347f8bec7fa589092ff7c35541d71cc5b
tatertot365 <tate.gillespie@gmail.com>
2024-05-23 12:30:57 -0600

    redoing of waitress configuration

--- api/api.py
+++ api/api.py
@@ -6,7 +6,7 @@ service = Service()
 
 @app.route("/")
 def home():
-    return "Welcome to the 47 Fund API version1.0!"
+    return "Welcome to the 47 Fund API"
 
 
 @app.route("/portfolio_total_return")
