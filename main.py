48dd99cf940af62cca746e96ce75f89232d0ac3d
tatertot365 <tate.gillespie@gmail.com>
2024-05-24 12:33:37 -0600

    added CORS to production app

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from api.api import app
 from waitress import serve
+from flask_cors import CORS
 
 if __name__ == "__main__":
+    CORS(app)
     serve(app, host="0.0.0.0", port=5000, url_scheme="https")
