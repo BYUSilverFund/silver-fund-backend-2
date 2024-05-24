1f74335fbcc514244718bbc34c989cca044ade09
tatertot365 <tate.gillespie@gmail.com>
2024-05-24 10:40:05 -0600

    added waitress back to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from api.api import app
+from waitress import serve
 
-#if __name__ == "__main__":
-    # serve(app, host="0.0.0.0", port=5000, url_scheme="https")
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000, url_scheme="https")
