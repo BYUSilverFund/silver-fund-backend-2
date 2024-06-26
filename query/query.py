fab739269181db2f82f53e8d7c37c4d31dfd4d63
andrewhall1124 <andrewmartinhall2@gmail.com>
2024-06-25 20:39:42 -0600

    Get all holdings df query + filtering out options

--- query/query.py
+++ query/query.py
@@ -159,6 +159,115 @@ class Query:
 
         return df
 
+    def get_all_holdings_df(self, fund: str, start_date: str, end_date: str) -> pd.DataFrame:
+        query_string = f'''
+            WITH
+                positions_query AS (
+                    SELECT
+                        date,
+                        fund,
+                        "Symbol" AS ticker,
+                        "Description" AS name,
+                        "Quantity"::DECIMAL AS shares_1,
+                        "MarkPrice"::DECIMAL AS price_1,
+                        "PositionValue"::DECIMAL AS value
+                    FROM positions
+                    WHERE fund = '{fund}' AND "AssetClass" != 'OPT'
+                    ORDER BY date
+                ),
+                positions_xf AS (
+                    SELECT
+                        *,
+                        CASE WHEN shares_1 > 0 THEN 1 ELSE -1 END AS side,
+                        LAG(shares_1) OVER (PARTITION BY ticker ORDER BY date) as shares_0,
+                        LAG(price_1) OVER (PARTITION BY ticker ORDER BY date) as price_0
+                    FROM positions_query
+                ),
+                dividends_query AS (
+                    SELECT
+                        date,
+                        fund,
+                        "Symbol" AS ticker,
+                        AVG("GrossRate"::DECIMAL) AS div_gross_rate,
+                        AVG("GrossAmount"::DECIMAL) AS div_gross_amount
+                    FROM dividends
+                    WHERE fund = '{fund}'
+                    GROUP BY date, fund, "Symbol"
+                ),
+                trades_query AS(
+                    SELECT
+                        date,
+                        fund,
+                        "Symbol" as ticker,
+                        SUM("Quantity"::DECIMAL) as shares_traded
+                    FROM trades
+                    WHERE fund = '{fund}'
+                    GROUP BY date, fund, "Symbol"
+                ),
+                join_table_1 AS(
+                    SELECT p.date,
+                           p.fund,
+                           p.ticker,
+                           p.name,
+                           p.shares_0,
+                           (p.shares_1 - COALESCE(shares_traded,0)) AS shares_1, --Adjusts shares_1 for trades made on that day
+                           t.shares_traded,
+                           p.price_0,
+                           p.price_1,
+                           p.side,
+                           p.value,
+                           d.div_gross_rate,
+                           d.div_gross_amount,
+                           p.side * ((p.price_1 * (p.shares_1 - COALESCE(shares_traded,0)) ) / (p.price_0 * p.shares_0) - 1) AS return -- + COALESCE(d.div_gross_amount, 0)
+                    FROM positions_xf p
+                    LEFT JOIN trades_query t ON p.date = t.date AND p.ticker = t.ticker AND p.fund = t.fund
+                    LEFT JOIN dividends_query d ON p.date = d.date AND p.ticker = d.ticker AND p.fund = d.fund
+                ),
+                bmk_query AS(
+                    SELECT 
+                        b.date,
+                        LAG(b.ending_value) OVER (ORDER BY b.date) AS starting_value,
+                        b.ending_value,
+                        d.div_gross_rate,
+                        d.div_gross_amount
+                    FROM benchmark b
+                    LEFT JOIN dividends_query d ON b.date = d.date AND d.ticker = 'IWV'
+                ),
+                bmk_xf AS(
+                    SELECT
+                        date,
+                        (ending_value ) / starting_value - 1 AS return -- + COALESCE(div_gross_rate, 0)
+                    FROM bmk_query
+                ),
+                join_table_2 AS(
+                    SELECT
+                       a.date,
+                       a.fund,
+                       a.ticker,
+                       a.name,
+                       a.shares_1 AS shares,
+                       a.price_1 AS price,
+                       a.value,
+                       a.side,
+                       a.return AS return,
+                       b.return AS bmk_return,
+                       c.return AS rf_return,
+                       a.return - c.return AS xs_return,
+                       b.return - c.return AS xs_bmk_return
+                    FROM join_table_1 a
+                    INNER JOIN bmk_xf b ON a.date = b.date
+                    INNER JOIN risk_free_rate c ON a.date = c.date
+                    WHERE a.return <> 0
+                        AND a.shares_1 <> 0
+                        AND a.date BETWEEN '{start_date}' AND '{end_date}'
+                )
+            SELECT * FROM join_table_2
+        '''
+
+        df = self.db.execute_query(query_string)
+
+        return df
+
     def get_holding_df(self, fund: str, ticker: str, start_date: str, end_date: str) -> pd.DataFrame:
         query_string = f'''
             WITH
@@ -173,7 +282,8 @@ class Query:
                         "PositionValue"::DECIMAL AS value
                     FROM positions
                     WHERE fund = '{fund}'
-                        AND "Symbol" = '{ticker}'
+                        AND "Symbol" = '{ticker}' 
+                        AND "AssetClass" != 'OPT'
                     ORDER BY date
                 ),
                 positions_xf AS (
@@ -275,6 +385,7 @@ class Query:
             SELECT DISTINCT("Symbol")
             FROM positions
             WHERE fund = '{fund}'
+                AND "AssetClass" != 'OPT'
                 AND date BETWEEN '{start_date}' AND '{end_date}'
         
         '''
@@ -288,7 +399,8 @@ class Query:
         SELECT date, fund, "Symbol"
         FROM positions
         WHERE fund = '{fund}'
-          AND date = (SELECT MAX(date) FROM positions);
+            AND "AssetClass" != 'OPT'
+            AND date = (SELECT MAX(date) FROM positions);
         '''
 
         df = self.db.execute_query(query_string)
