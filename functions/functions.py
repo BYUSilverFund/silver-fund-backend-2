0f6ffb48833ee2875505682f806b6d0c8fcf2226
andrewhall1124 <andrewmartinhall2@gmail.com>
2024-07-29 18:40:31 -0600

    Information ratio fix

--- functions/functions.py
+++ functions/functions.py
@@ -186,7 +186,9 @@ def information_ratio(returns: pd.Series, bmk_returns: pd.Series, rf_returns: pd
     xs_returns = returns - rf_returns
     xs_bmk_returns = bmk_returns - rf_returns
 
-    port_alpha = alpha(xs_returns, xs_bmk_returns)
+    num_days = returns.count()
+
+    port_alpha = alpha(xs_returns, xs_bmk_returns) / num_days
     port_tracking_error = tracking_error(returns, bmk_returns)
 
     ratio = port_alpha / port_tracking_error
