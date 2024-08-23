f778bb06460621af596c9b85ae75f9b16e195be1
tatertot365 <tate.gillespie@gmail.com>
2024-08-23 14:55:14 -0600

    updated getting cron logs query and updated log statements

--- chron/main.py
+++ chron/main.py
@@ -16,7 +16,7 @@ def run():
         token = config[fund]['token']
         query_types = config[fund]['queries'].keys()
 
-        print(f"Beginning {fund} fund ETL")
+        # print(f"Beginning {fund} fund ETL")
         cron_log_string += f"{fund}: "
 
         for query_type in query_types:
@@ -24,35 +24,35 @@ def run():
                 query_id = config[fund]['queries'][query_type]
 
                 # Extract
-                print(f"Executing IBKR {query_type} query")
+                # print(f"Executing IBKR {query_type} query")
                 cron_log_string += f"{query_type} extracted, "
                 raw_data = ibkr_query(token, query_id)
 
                 # Transform
-                print(f"Transforming the {query_type} data")
-                cron_log_string += f"{query_type} transformed, "
+                # print(f"Transforming the {query_type} data")
+                cron_log_string += f"transformed, "
                 transformed_data = transform(raw_data, fund, query_type)
 
                 # Load
                 database.load_df(transformed_data, query_type)
-                cron_log_string += f"{query_type} loaded. \n"
-                print(f"Data loaded into the {query_type} table in the database.")
+                cron_log_string += f"loaded. \n"
+                # print(f"Data loaded into the {query_type} table in the database.")
 
             except Exception as e:
-                print(f"Error loading {fund} {query_type} data: {e}")
+                # print(f"Error loading {fund} {query_type} data: {e}")
                 cron_log_string += f"Error loading {fund} {query_type} data: {e}\n"
 
             
 
     try:
-        print("Updating risk free rate")
-        cron_log_string += "Updated risk free rate\n"
+        # print("Updating risk free rate")
+        cron_log_string += "Updated risk-free rate.\n"
         raw_rf = fred_query()
         transformed_rf = transform_rf(raw_rf)
         database.load_df(transformed_rf, 'risk_free_rate')
 
-        print("Updating benchmark")
-        cron_log_string += "Updated benchmark\n"
+        # print("Updating benchmark")
+        cron_log_string += "Updated benchmark.\n"
         bmk_token = config['undergrad']['token']
         bmk_query_id = config['undergrad']['queries']['positions']
 
@@ -61,7 +61,7 @@ def run():
         database.load_df(transformed_bmk, 'benchmark')
 
     except Exception as e:
-        print(f"Error updating risk free rate or benchmark: {e}")
+        # print(f"Error updating risk free rate or benchmark: {e}")
         cron_log_string += f"Error updating risk free rate or benchmark: {e}\n"
 
     database.load_cron_log(cron_log_string)
