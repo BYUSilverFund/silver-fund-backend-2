7bd3fee51de819ac5b4c584cddeaff745d1c7fde
Andrew Hall <andrewmartinhall2@gmail.com>
2024-08-30 08:24:24 -0600

    Queries are updated to take in first and last day the positions was held.

--- server/query.py
+++ server/query.py
@@ -156,6 +156,7 @@ class Query:
                         AVG("GrossAmount"::DECIMAL) AS div_gross_amount -- Sometimes dividends gets double counted
                     FROM dividends
                     WHERE fund = '{fund}'
+                        AND "Symbol" = '{ticker}'
                     GROUP BY date, fund, "Symbol"
                 ),
                 trades_query AS(
@@ -297,31 +298,21 @@ class Query:
 
     def get_all_holdings_df(self, fund: str, start: str, end: str):
         query_string = f'''
-        WITH
+            WITH
                 positions_query AS (
                     SELECT
                         date,
                         fund,
                         "Symbol" AS ticker,
                         "Description" AS name,
+                        CASE WHEN "Quantity"::DECIMAL > 0 THEN 1 ELSE -1 END AS side,
                         "Quantity"::DECIMAL AS shares_1,
                         "MarkPrice"::DECIMAL AS price_1,
-                        "PositionValue"::DECIMAL AS value_1,
                         "FXRateToBase":: DECIMAL AS fx_rate_1
                     FROM positions
                     WHERE fund = '{fund}'
                         AND "AssetClass" != 'OPT'
-                    ORDER BY date
-                ),
-                positions_xf AS (
-                    SELECT
-                        *,
-                        CASE WHEN shares_1 > 0 THEN 1 ELSE -1 END AS side,
-                        LAG(shares_1) OVER (PARTITION BY ticker ORDER BY date) as shares_0,
-                        LAG(price_1) OVER (PARTITION BY ticker ORDER BY date) as price_0,
-                        LAG(value_1) OVER (PARTITION BY ticker ORDER BY date) as value_0,
-                        LAG(fx_rate_1) OVER (PARTITION BY ticker ORDER BY date) as fx_rate_0
-                    FROM positions_query
+                    ORDER BY ticker, date
                 ),
                 dividends_query AS (
                     SELECT
@@ -339,10 +330,12 @@ class Query:
                         date,
                         fund,
                         "Symbol" as ticker,
-                        SUM("Quantity"::DECIMAL) as shares_traded
+                        CASE WHEN "Buy/Sell" = 'BUY' THEN 1 ELSE -1 END AS trade_type,
+                        SUM("Quantity"::DECIMAL) AS shares_traded,
+                        AVG("TradePrice"::DECIMAL) as trade_price
                     FROM trades
                     WHERE fund = '{fund}'
-                    GROUP BY date, fund, "Symbol"
+                    GROUP BY date, fund, "Symbol", "Buy/Sell"
                 ),
                 nav_query AS(
                     SELECT
@@ -360,57 +353,109 @@ class Query:
                         LAG(total_stock_1) OVER (ORDER BY date) AS total_stock_0
                     FROM nav_query
                 ),
-                join_table_1 AS(
-                    SELECT p.date,
-                           p.fund,
-                           p.ticker,
-                           p.name,
-                           p.value_0 / n.total_stock_0 AS weight_0,
-                           p.value_1 / n.total_stock_1 AS weight_1,
-                           p.shares_0,
-                           (p.shares_1 - COALESCE(shares_traded,0)) AS shares_1, --Adjusts shares_1 for trades made on that day
-                           t.shares_traded,
-                           p.price_0,
-                           p.price_1,
-                           p.side,
-                           p.value_0,
-                           p.value_1,
-                           d.div_gross_rate,
-                           d.div_gross_amount AS dividends,
-                           p.side * ((p.price_1 * (p.shares_1 - COALESCE(shares_traded,0)) * fx_rate_1 ) / (p.price_0 * p.shares_0 * fx_rate_0) - 1) AS return,
-                           p.side * (((p.price_1 * (p.shares_1 - COALESCE(shares_traded,0)) + COALESCE(d.div_gross_amount, 0)) * fx_rate_1) / (p.price_0 * p.shares_0 * fx_rate_0) - 1) AS div_return
-                    FROM positions_xf p
-                    LEFT JOIN trades_query t ON p.date = t.date AND p.ticker = t.ticker AND p.fund = t.fund
+                join_table_1 AS( -- Merge trades and dividends into positions
+                    SELECT
+                        COALESCE(p.date, t.date) AS date,
+                        COALESCE(p.fund, t.fund) AS fund,
+                        COALESCE(p.ticker, t.ticker) AS ticker,
+                        p.side,
+                        p.shares_1,
+                        p.price_1,
+                        p.fx_rate_1,
+                        t.trade_type,
+                        COALESCE(t.shares_traded, 0) AS shares_traded,
+                        t.trade_price,
+                        COALESCE(d.div_gross_rate,0) as div_gross_rate,
+                        COALESCE(d.div_gross_amount, 0) AS dividends
+                    FROM positions_query p
+                    FULL JOIN trades_query t ON p.date = t.date AND p.ticker = t.ticker AND p.fund = t.fund
                     LEFT JOIN dividends_query d ON p.date = d.date AND p.ticker = d.ticker AND p.fund = d.fund
+                    ORDER BY ticker, COALESCE(p.date, t.date)
+                ),
+                join_table_2 AS( -- Coalesce and lag missing values
+                    SELECT
+                        date,
+                        fund,
+                        ticker,
+                        COALESCE(LAG(side) OVER (PARTITION BY ticker ORDER BY date), side) AS side,
+                        COALESCE(LAG(shares_1) OVER (PARTITION BY ticker ORDER BY date), shares_traded) AS shares_0,
+                        COALESCE(shares_1 - CASE WHEN trade_type = 1 THEN 0 ELSE shares_traded END, shares_traded * trade_type) AS shares_1,
+                        COALESCE(LAG(price_1) OVER (PARTITION BY ticker ORDER BY date), trade_price) AS price_0,
+                        COALESCE(price_1, trade_price) AS price_1,
+                        COALESCE(LAG(fx_rate_1) OVER (PARTITION BY ticker ORDER BY date), fx_rate_1) AS fx_rate_0,
+                        COALESCE(fx_rate_1, LAG(fx_rate_1) OVER (PARTITION BY ticker ORDER BY date)) AS fx_rate_1,
+                        trade_type,
+                        shares_traded,
+                        trade_price,
+                        div_gross_rate,
+                        dividends
+                    FROM join_table_1
+                ),
+                join_table_3 AS(
+                    SELECT
+                        date,
+                        fund,
+                        ticker,
+                        side,
+                        shares_0,
+                        shares_1,
+                        price_0,
+                        price_1,
+                        fx_rate_0,
+                        fx_rate_1,
+                        shares_0 * price_0 * fx_rate_0 AS value_0,
+                        shares_1 * price_1 * fx_rate_1 AS value_1,
+                        trade_type,
+                        shares_traded,
+                        trade_price,
+                        div_gross_rate,
+                        dividends
+                    FROM join_table_2
+                ),
+                join_table_4 AS( -- Compute weights and returns
+                    SELECT
+                        p.date,
+                        p.fund,
+                        ticker,
+                        p.side,
+                        shares_1,
+                        price_1,
+                        fx_rate_1,
+                        value_1,
+                        n.total_stock_1,
+                        value_1 / n.total_stock_1 AS weight_1,
+                        div_gross_rate,
+                        dividends,
+                        side * value_1 / value_0 - 1 AS return,
+                        side * (value_1 + dividends) / value_0 - 1 AS div_return
+                    FROM join_table_3 p
                     LEFT JOIN nav_xf n ON p.date = n.date AND p.fund = n.fund
                 ),
-                join_table_2 AS(
+                join_table_5 AS( -- Compute excess returns
                     SELECT
-                       a.date,
-                       a.fund,
-                       a.ticker,
-                       a.name,
-                       a.weight_1 AS weight,
-                       a.shares_1 AS shares,
-                       a.price_1 AS price,
-                       a.value_1 AS value,
-                       a.side,
-                       a.div_gross_rate / a.price_1 AS dividend_yield,
-                       a.dividends,
-                       a.return,
-                       a.div_return,
-                       COALESCE(c.return, LAG(c.return) OVER (ORDER BY a.ticker, c.date)) AS rf_return,
-                       a.return - COALESCE(c.return, LAG(c.return) OVER (ORDER BY a.ticker, c.date)) AS xs_return,
-                       a.div_return - COALESCE(c.return, LAG(c.return) OVER (ORDER BY a.ticker, c.date)) AS xs_div_return
-                    FROM join_table_1 a
+                    a.date,
+                    a.fund,
+                    a.ticker,
+                    a.side,
+                    a.price_1 AS price,
+                    a.shares_1 AS shares,
+                    a.fx_rate_1 AS fx_rate,
+                    a.value_1 AS value,
+                    a.weight_1 AS weight,
+                    a.div_gross_rate / a.price_1 AS dividend_yield,
+                    a.dividends,
+                    a.return,
+                    a.div_return,
+                    COALESCE(c.return, LAG(c.return) OVER (ORDER BY c.date)) AS rf_return,
+                    a.return - COALESCE(c.return, LAG(c.return) OVER (ORDER BY c.date)) AS xs_return,
+                    a.div_return - COALESCE(c.return, LAG(c.return) OVER (ORDER BY c.date)) AS xs_div_return
+                    FROM join_table_4 a
                     LEFT JOIN risk_free_rate c ON a.date = c.date
                     WHERE a.return <> 0
-                        AND a.shares_1 <> 0
                         AND a.date BETWEEN '{start}' AND '{end}'
-                    ORDER BY a.ticker, a.date
+                    ORDER BY ticker, a.date
                 )
-                SELECT * FROM join_table_2
-        ;
+                SELECT * FROM join_table_5;
         '''
 
         df = self.db.execute_query(query_string)
