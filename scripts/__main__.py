3e4020cd2932d9bc396ea5f02ebfb13e95b05fcc
tatertot365 <tate.gillespie@gmail.com>
2024-08-30 12:19:26 -0600

    cleaned up cron string for ibkr and added corn string for S3 positions

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -37,10 +37,13 @@ def upload_fund_positions(df: pd.DataFrame, fund: str) -> None:
 
 
 def main() -> None:
+    cron_log_string = ""
     for fund in FUNDS:
         df = query_fund_positions(fund)
         upload_fund_positions(df, fund)
+        cron_log_string += f"{fund} uploaded to S3. "
 
+    Database().load_cron_log(cron_log_string)
 if __name__ == "__main__":
     main()
     
