e0f8538595f5b47db9a458871f4c21ae637a1a64
tatertot365 <tate.gillespie@gmail.com>
2024-09-11 17:00:06 -0600

    prepared for elastic beanstalk

--- application.py
+++ application.py
@@ -10,22 +10,21 @@ import boto3
 
 application = Flask(__name__)
 service = Service()
-CORS(application)
-Talisman(application)
 
+# CORS(application)
+# Talisman(application)
 
 def check_user(username):
-    cognito = boto3.client("cognito-idp", region_name="us-west-2", aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-                           aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
+    cognito = boto3.client("cognito-idp", region_name="us-west-2", aws_access_key_id=os.getenv("COGNITO_ACCESS_KEY_ID"),
+                           aws_secret_access_key=os.getenv("COGNITO_SECRET_ACCESS_KEY"))
     try:
         response = cognito.admin_get_user(
-            UserPoolId=os.getenv("AWS_USER_POOL_ID"),
+            UserPoolId=os.getenv("COGNITO_USER_POOL_ID"),
             Username=username
         )
         return True
     except Exception as e:
         return False
-
 
 @application.before_request
 def before_request():
@@ -42,162 +41,165 @@ def before_request():
             return json.dumps({"error": "Invalid authorization token"}), 401
 
 
-@application.route("/")
+@application.route("/", methods=["GET"])
 def home():
-    return "Welcome to the 47 Fund API v1.0"
-
-
-@application.route("/test")
-def test():
-    parameter = request.args.get("fund")
-    return json.dumps({"fund": parameter})
-
-
-@application.route("/fund_summary", methods=["GET"])
-def fund_summary():
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.fund_summary(start_date, end_date)
-    return response
-
-
-@application.route("/fund_chart", methods=["GET"])
-def fund_chart():
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.fund_chart_data(start_date, end_date)
-    return response
-
-
-@application.route("/portfolio_summary", methods=["GET"])
-def portfolio_summary():
-    fund = request.args.get("fund")
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.portfolio_summary(fund, start_date, end_date)
-    return response
-
-
-@application.route("/portfolio_chart", methods=["GET"])
-def portfolio_chart():
-    fund = request.args.get("fund")
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.portfolio_chart_data(fund, start_date, end_date)
-    return response
-
-
-@application.route("/holding_chart", methods=["GET"])
-def holding_chart():
-    fund = request.args.get("fund")
-    ticker = request.args.get("ticker")
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.holding_chart_data(fund, ticker, start_date, end_date)
-    return response
-
-
-@application.route("/benchmark_chart", methods=["GET"])
-def benchmark_chart():
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.benchmark_chart_data(start_date, end_date)
-    return response
-
-
-@application.route("/all_portfolios_summary", methods=["GET"])
-def all_portfolios_summary():
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.all_portfolios_summary(start_date, end_date)
-
-    return response
-
-
-@application.route("/all_holdings_summary", methods=["GET"])
-def all_holdings_summary():
-    fund = request.args.get("fund")
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.all_holdings_summary(fund, start_date, end_date)
-
-    return response
-
-
-@application.route("/holding_summary", methods=["GET"])
-def holding_summary():
-    fund = request.args.get("fund")
-    ticker = request.args.get("ticker")
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.holding_summary(fund, ticker, start_date, end_date)
-
-    return response
-
-
-@application.route("/holding_dividends", methods=["GET"])
-def holding_dividends():
-    fund = request.args.get("fund")
-    ticker = request.args.get("ticker")
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.holding_dividends(fund, ticker, start_date, end_date)
-
-    return response
-
-
-@application.route("/holding_trades", methods=["GET"])
-def holding_trades():
-    fund = request.args.get("fund")
-    ticker = request.args.get("ticker")
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.holding_trades(fund, ticker, start_date, end_date)
-
-    return response
-
-
-@application.route("/benchmark_summary", methods=["GET"])
-def benchmark_summary():
-    start_date = request.args.get("start")
-    end_date = request.args.get("end")
-
-    response = service.benchmark_summary(start_date, end_date)
-
-    return response
-
-@application.route("/cron_logs", methods=["GET"])
-def cron_log():
-    response = service.cron_logs()
-    return response
+    return "Welcome to the 47 Fund Elastic Beanstalk API v1.0"
 
 @application.route("/health_check", methods=["GET"])
 def health_check():
     response = json.dumps({"message": "Instances are healthy"}), 200
     return response
 
+@application.route("/test")
+def test():
+    parameter = request.args.get("fund")
+    return json.dumps({"fund": parameter})
 
-if __name__ == '__main__':
-    # Check if the environment is set to production
-    # testing or development
-    environment = os.getenv('ENVIRONMENT')
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+
+
+# @application.route("/fund_summary", methods=["GET"])
+# def fund_summary():
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.fund_summary(start_date, end_date)
+#     return response
+
+
+# @application.route("/fund_chart", methods=["GET"])
+# def fund_chart():
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.fund_chart_data(start_date, end_date)
+#     return response
+
+
+# @application.route("/portfolio_summary", methods=["GET"])
+# def portfolio_summary():
+#     fund = request.args.get("fund")
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.portfolio_summary(fund, start_date, end_date)
+#     return response
+
+
+# @application.route("/portfolio_chart", methods=["GET"])
+# def portfolio_chart():
+#     fund = request.args.get("fund")
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.portfolio_chart_data(fund, start_date, end_date)
+#     return response
+
+
+# @application.route("/holding_chart", methods=["GET"])
+# def holding_chart():
+#     fund = request.args.get("fund")
+#     ticker = request.args.get("ticker")
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.holding_chart_data(fund, ticker, start_date, end_date)
+#     return response
+
+
+# @application.route("/benchmark_chart", methods=["GET"])
+# def benchmark_chart():
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.benchmark_chart_data(start_date, end_date)
+#     return response
+
+
+# @application.route("/all_portfolios_summary", methods=["GET"])
+# def all_portfolios_summary():
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.all_portfolios_summary(start_date, end_date)
+
+#     return response
+
+
+# @application.route("/all_holdings_summary", methods=["GET"])
+# def all_holdings_summary():
+#     fund = request.args.get("fund")
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.all_holdings_summary(fund, start_date, end_date)
+
+#     return response
+
+
+# @application.route("/holding_summary", methods=["GET"])
+# def holding_summary():
+#     fund = request.args.get("fund")
+#     ticker = request.args.get("ticker")
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.holding_summary(fund, ticker, start_date, end_date)
+
+#     return response
+
+
+# @application.route("/holding_dividends", methods=["GET"])
+# def holding_dividends():
+#     fund = request.args.get("fund")
+#     ticker = request.args.get("ticker")
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.holding_dividends(fund, ticker, start_date, end_date)
+
+#     return response
+
+
+# @application.route("/holding_trades", methods=["GET"])
+# def holding_trades():
+#     fund = request.args.get("fund")
+#     ticker = request.args.get("ticker")
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.holding_trades(fund, ticker, start_date, end_date)
+
+#     return response
+
+
+# @application.route("/benchmark_summary", methods=["GET"])
+# def benchmark_summary():
+#     start_date = request.args.get("start")
+#     end_date = request.args.get("end")
+
+#     response = service.benchmark_summary(start_date, end_date)
+
+#     return response
+
+# @application.route("/cron_logs", methods=["GET"])
+# def cron_log():
+#     response = service.cron_logs()
+#     return response
+
+
+# if __name__ == '__main__':
+#     # Check if the environment is set to production
+#     # testing or development
+#     environment = os.getenv('ENVIRONMENT')
     
-    if environment == 'PRODUCTION':
-        serve(application, host="0.0.0.0", port=5000, url_scheme="https")
+#     if environment == 'PRODUCTION':
+#         serve(application, host="0.0.0.0", port=5000, url_scheme="https")
 
-    elif environment == 'DEVELOPMENT':
-        application.run(debug=True, port=8080)
+#     elif environment == 'DEVELOPMENT':
+#         application.run(debug=True, port=8080)
 
-    else:
-        print("You have not set your ENVIRONMENT in .env")
+#     else:
+#         print("You have not set your ENVIRONMENT in .env")
--- scripts/__main__.py
+++ scripts/__main__.py
@@ -22,8 +22,8 @@ def query_fund_positions(fund: str) -> pd.DataFrame:
 
 def upload_fund_positions(df: pd.DataFrame, fund: str) -> None:
     s3 = boto3.client('s3',
-                      aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                      aws_access_key_id=os.getenv('COGNITO_ACCESS_KEY_ID'),
+                        aws_secret_access_key=os.getenv('COGNITO_SECRET_ACCESS_KEY'),
                         region_name='us-west-2')
 
     csv_buffer = StringIO()
