f5daa8a8d343d6acb46ec230bfd0e27ad95ccb27
tatertot365 <tate.gillespie@gmail.com>
2024-09-12 19:59:22 -0600

    added other api endpoints

--- application.py
+++ application.py
@@ -45,149 +45,151 @@ def before_request():
 def home():
     return "Welcome to the 47 Fund Elastic Beanstalk API v1.0"
 
+
 @application.route("/health_check", methods=["GET"])
 def health_check():
     response = json.dumps({"message": "Instances are healthy"}), 200
     return response
+
 
 @application.route("/test")
 def test():
     parameter = request.args.get("fund")
     return json.dumps({"fund": parameter})
 
+
+@application.route("/fund_summary", methods=["GET"])
+def fund_summary():
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.fund_summary(start_date, end_date)
+    return response
+
+
+@application.route("/fund_chart", methods=["GET"])
+def fund_chart():
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.fund_chart_data(start_date, end_date)
+    return response
+
+
+@application.route("/portfolio_summary", methods=["GET"])
+def portfolio_summary():
+    fund = request.args.get("fund")
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.portfolio_summary(fund, start_date, end_date)
+    return response
+
+
+@application.route("/portfolio_chart", methods=["GET"])
+def portfolio_chart():
+    fund = request.args.get("fund")
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.portfolio_chart_data(fund, start_date, end_date)
+    return response
+
+
+@application.route("/holding_chart", methods=["GET"])
+def holding_chart():
+    fund = request.args.get("fund")
+    ticker = request.args.get("ticker")
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.holding_chart_data(fund, ticker, start_date, end_date)
+    return response
+
+
+@application.route("/benchmark_chart", methods=["GET"])
+def benchmark_chart():
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.benchmark_chart_data(start_date, end_date)
+    return response
+
+
+@application.route("/all_portfolios_summary", methods=["GET"])
+def all_portfolios_summary():
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.all_portfolios_summary(start_date, end_date)
+
+    return response
+
+
+@application.route("/all_holdings_summary", methods=["GET"])
+def all_holdings_summary():
+    fund = request.args.get("fund")
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.all_holdings_summary(fund, start_date, end_date)
+
+    return response
+
+
+@application.route("/holding_summary", methods=["GET"])
+def holding_summary():
+    fund = request.args.get("fund")
+    ticker = request.args.get("ticker")
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.holding_summary(fund, ticker, start_date, end_date)
+
+    return response
+
+
+@application.route("/holding_dividends", methods=["GET"])
+def holding_dividends():
+    fund = request.args.get("fund")
+    ticker = request.args.get("ticker")
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.holding_dividends(fund, ticker, start_date, end_date)
+
+    return response
+
+
+@application.route("/holding_trades", methods=["GET"])
+def holding_trades():
+    fund = request.args.get("fund")
+    ticker = request.args.get("ticker")
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.holding_trades(fund, ticker, start_date, end_date)
+
+    return response
+
+
+@application.route("/benchmark_summary", methods=["GET"])
+def benchmark_summary():
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
+
+    response = service.benchmark_summary(start_date, end_date)
+
+    return response
+
+@application.route("/cron_logs", methods=["GET"])
+def cron_log():
+    response = service.cron_logs()
+    return response
+
 if __name__ == "__main__":
     application.debug = True
     application.run()
-
-
-# @application.route("/fund_summary", methods=["GET"])
-# def fund_summary():
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.fund_summary(start_date, end_date)
-#     return response
-
-
-# @application.route("/fund_chart", methods=["GET"])
-# def fund_chart():
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.fund_chart_data(start_date, end_date)
-#     return response
-
-
-# @application.route("/portfolio_summary", methods=["GET"])
-# def portfolio_summary():
-#     fund = request.args.get("fund")
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.portfolio_summary(fund, start_date, end_date)
-#     return response
-
-
-# @application.route("/portfolio_chart", methods=["GET"])
-# def portfolio_chart():
-#     fund = request.args.get("fund")
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.portfolio_chart_data(fund, start_date, end_date)
-#     return response
-
-
-# @application.route("/holding_chart", methods=["GET"])
-# def holding_chart():
-#     fund = request.args.get("fund")
-#     ticker = request.args.get("ticker")
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.holding_chart_data(fund, ticker, start_date, end_date)
-#     return response
-
-
-# @application.route("/benchmark_chart", methods=["GET"])
-# def benchmark_chart():
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.benchmark_chart_data(start_date, end_date)
-#     return response
-
-
-# @application.route("/all_portfolios_summary", methods=["GET"])
-# def all_portfolios_summary():
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.all_portfolios_summary(start_date, end_date)
-
-#     return response
-
-
-# @application.route("/all_holdings_summary", methods=["GET"])
-# def all_holdings_summary():
-#     fund = request.args.get("fund")
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.all_holdings_summary(fund, start_date, end_date)
-
-#     return response
-
-
-# @application.route("/holding_summary", methods=["GET"])
-# def holding_summary():
-#     fund = request.args.get("fund")
-#     ticker = request.args.get("ticker")
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.holding_summary(fund, ticker, start_date, end_date)
-
-#     return response
-
-
-# @application.route("/holding_dividends", methods=["GET"])
-# def holding_dividends():
-#     fund = request.args.get("fund")
-#     ticker = request.args.get("ticker")
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.holding_dividends(fund, ticker, start_date, end_date)
-
-#     return response
-
-
-# @application.route("/holding_trades", methods=["GET"])
-# def holding_trades():
-#     fund = request.args.get("fund")
-#     ticker = request.args.get("ticker")
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.holding_trades(fund, ticker, start_date, end_date)
-
-#     return response
-
-
-# @application.route("/benchmark_summary", methods=["GET"])
-# def benchmark_summary():
-#     start_date = request.args.get("start")
-#     end_date = request.args.get("end")
-
-#     response = service.benchmark_summary(start_date, end_date)
-
-#     return response
-
-# @application.route("/cron_logs", methods=["GET"])
-# def cron_log():
-#     response = service.cron_logs()
-#     return response
 
 
 # if __name__ == '__main__':
