99c718d065bab95be86b8e519701bca578ce5fe9
Andrew Hall <andrewmartinhall2@gmail.com>
2024-09-25 11:44:29 -0600

    Stubs for version 2 of chron job

---
+++ chron2/config.py
@@ -0,0 +1,42 @@
+config = {
+    "grad": {
+        "token": 819997527277769608738195,
+        "queries": {
+            "nav": 993010,
+            "delta_nav": 993013,
+            "positions": 993015,
+            "dividends": 993011,
+            "trades": 993012,
+        }
+    },
+    "undergrad": {
+        "token": 981720897048992989674255,
+        "queries": {
+            "nav": 989615,
+            "delta_nav": 989561,
+            "positions": 989564,
+            "dividends": 989565,
+            "trades": 989567,
+        }
+    },
+    "quant": {
+        "token": 2755429139866727041920,
+        "queries": {
+            "nav": 1029491,
+            "delta_nav": 1029495,
+            "positions": 1029501,
+            "dividends": 1029496,
+            "trades": 1029498,
+        }
+    },
+    "brigham_capital": {
+        "token": 170229039687872048124200,
+        "queries": {
+            "nav": 989606,
+            "delta_nav": 989601,
+            "positions": 989602,
+            "dividends": 989603,
+            "trades": 989605,
+        }
+    },
+}
