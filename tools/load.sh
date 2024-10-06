65b3160dcd09e065a0ab7aa2fcfb2e90a6cb90ed
Andrew Hall <andrewmartinhall2@gmail.com>
2024-10-06 10:42:34 -0600

    Load into database tool

--- tools/load.sh
+++ tools/load.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DIRECTORY="files"
 
-for filepath in "$DIRECTORY"/*.csv; do
-    python -m load_csv "$filepath"
+tables=("positions") # "dividends" "nav" "delta_nav" "trades")
+for table in "${tables[@]}"; do
+    for filepath in "$table"/*.csv; do
+        python -m load_csv "$filepath" "$table"
+    done
 done
