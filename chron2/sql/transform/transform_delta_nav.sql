68dd277af8b4d8db910b1efee26dc2d53b82421a
Andrew Hall <andrewmartinhall2@gmail.com>
2024-10-09 15:17:25 -0600

    Transform step done.

---
+++ chron2/sql/transform/transform_delta_nav.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS "XF_{{stage_table}}";
+CREATE TABLE "{{xf_table}}" AS 
+SELECT DISTINCT
+    TO_DATE("FromDate", 'YYYYMMDD')             AS CALDT,
+    '{{fund}}'                                  AS FUND,
+    "StartingValue"::NUMERIC(18,6)              AS STARTING_VALUE,
+    "EndingValue"::NUMERIC(18,6)                AS ENDING_VALUE,
+    "DepositsWithdrawals"::NUMERIC(18,6)        AS DEPOSITS_WITHDRAWALS
+FROM "{{stage_table}}"
+WHERE "ClientAccountID" != 'ClientAccountID';
