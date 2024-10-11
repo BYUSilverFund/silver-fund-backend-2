2ee1bf8e780e3baf8444bd514093bb6dd2feea00
Andrew Hall <andrewmartinhall2@gmail.com>
2024-10-10 18:44:27 -0600

    Realized there's an easier way to do benchmark

--- chron2/sql/transform/transform_benchmark.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS "{{xf_table}}";
-CREATE TABLE "{{xf_table}}" AS 
-SELECT DISTINCT
-    TO_DATE("ReportDate", 'YYYYMMDD')       AS CALDT,
-    "Symbol"                                AS TICKER,
-    "MarkPrice"::NUMERIC(18,6)              AS ENDING_VALUE
-FROM "{{stage_table}}"
-WHERE "ClientAccountID" != 'ClientAccountID'
-AND "Symbol" = 'IWV';
