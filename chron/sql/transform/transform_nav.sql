4451abcc89f11d247dd5c0d74a224570eac9622f
tatertot365 <tate.gillespie@gmail.com>
2024-12-31 10:18:47 -0600

    added carchar to other report date queries

--- chron/sql/transform/transform_nav.sql
+++ chron/sql/transform/transform_nav.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS "XF_{{stage_table}}";
 CREATE TABLE "{{xf_table}}" AS 
 SELECT DISTINCT
-    TO_DATE("ReportDate", 'YYYYMMDD')   AS CALDT,
+    TO_DATE("ReportDate"::VARCHAR(255), 'YYYYMMDD')   AS CALDT,
     '{{fund}}'                          AS FUND,
     "Stock"::NUMERIC(18,6)              AS STOCK
 FROM "{{stage_table}}"
