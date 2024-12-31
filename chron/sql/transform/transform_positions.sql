e2db2642aff1417602328fad54dcb7825c5266de
tatertot365 <tate.gillespie@gmail.com>
2024-12-31 10:21:57 -0600

    added varchar to FromDate in transform

--- chron/sql/transform/transform_positions.sql
+++ chron/sql/transform/transform_positions.sql
@@ -1,15 +1,7 @@
 DROP TABLE IF EXISTS "XF_{{stage_table}}";
 CREATE TABLE "{{xf_table}}" AS 
 SELECT DISTINCT
-<<<<<<< HEAD
-<<<<<<< HEAD
-    TO_DATE("ReportDate"::TEXT, 'YYYYMMDD') AS CALDT,
-=======
     TO_DATE("ReportDate"::VARCHAR(255), 'YYYYMMDD')       AS CALDT,
->>>>>>> 4451abc (added carchar to other report date queries)
-=======
-    TO_DATE("ReportDate"::VARCHAR(255), 'YYYYMMDD')       AS CALDT,
->>>>>>> 4451abc (added carchar to other report date queries)
     '{{fund}}'                              AS FUND,
     "Symbol"                                AS TICKER,
     "AssetClass"                            AS ASSET_CLASS,
